0ac7b5261d0a5945598de3739770048df57f5220
Mike Tritsch <122573481+trickortritsch@users.noreply.github.com>
2023-10-09 12:44:22 -0500

    added marker system framework w/ custom icon

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,20 @@
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -34.397, lng: 150.644},
+        center: {lat: 30.261407876987132, lng: -97.74064430143332},
         zoom: 8
-    });
-}
+    })
+
+// Custom markers
+    var markerOptions = {
+        position: new google.maps.LatLng(30.380399007158175, -97.7152186045917),
+        map: map,
+        icon: './assets/images/beer-mug.png'
+    }
+
+    let marker = new google.maps.Marker(markerOptions);
+    marker.setMap(map);
+
+};
 
 
 
