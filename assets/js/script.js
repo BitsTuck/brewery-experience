2f94e004e1dea42080ca773dcf778188399a3a23
Mike Tritsch <122573481+trickortritsch@users.noreply.github.com>
2023-10-09 10:40:11 -0500

    rendered Google map API, added general CSS/JS tags in HTML, created DIV for map

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 8
+    });
+}
+
+
+
+
+
+
+
+
+// Map ID: 40a6433050f67110
+// API key: AIzaSyAgUNZngbasW9LtIjfNm5M_ICxJxjlCIkc
