28a7d26af04ad74605467634711605f45ecdf26c
martin sandoval <martinsandoval@martins-Air.attlocal.net>
2023-10-09 12:43:18 -0500

    api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,14 @@
 
+function getParams(){
+var searchparamArr = document.location.search.split('&');
+
+var query = searchparamArr[0].split('=').pop();
+var format = searchparamArr[1].split('=').pop();
+
+searchbrew(query, format);
+
+}
+function searchbrew(query){
+    var locQueryUrl = 'https://api.openbrewerydb.org/v1/breweries/search?query='
+
+}
